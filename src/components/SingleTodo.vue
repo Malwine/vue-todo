6e26135bd9f944ccf3b62021d3fd16211bd2912c
Malwine <malwine@mailbox.org>
2024-01-05 20:14:39 +0100

    Start looping through single todos

---
+++ src/components/SingleTodo.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Todo Single</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <p class="display-3">
+          Vue Todos
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <p>Add create form</p>
+    </div>
+    <div class="row">
+      <div class="col-12 col-lg-6">
+        <ul class="list-group">
+          <SingleTodo v-for="(todo, index) in todos" 
+          :key="index" 
+          :text="todo.text"
+          :completed="todo.completed"/>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SingleTodo from "./SingleTodo.vue"
+export default {
+  components: {
+    SingleTodo,
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: "Learn Vue", completed: true },
+        { id: 2, text: "Learn many things with Hitesh", completed: false },
+        { id: 3, text: "Have fun", completed: false },
+      ]
+    };
+  },
+  methods: {
+    addTodo(newTodo){
+      this.todos.push({
+        text: newTodo, completed: false});
+    },
+    toggleTodo(todo){
+      todo.completed = !todo.completed;
+    }, 
+    editTodo(todo, newText){
+      todo.text = newText;
+    }, 
+    deleteTodo(deleteTodo){
+      this.todos = this.todos.filter(todo => todo !== deleteTodo)
+    }
+  },
+};
+</script>
+
+<style>
+</style>
